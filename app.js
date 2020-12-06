acd306415b8ac6acf101f1547eb49da47d19877e
Konstantinos Tserpes <tserpes@hua.gr>
2020-12-06 09:55:29 +0200

    class version

--- app.js
+++ app.js
@@ -1,21 +1,47 @@
+process.title = 'node_restful';
+
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('students.sqlite');
 
 const express = require('express');
 const app = express();
+const cors = require('cors');
+app.use(cors());
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
  
-app.get('/', async (req, res)=>{
+app.get('/students/', async (req, res)=>{
 	const q = `select rowid,fname,lname from students`;
 	try{
 		const results = await query(q);
-		res.send(results);
+		res.json(results);
 	}catch(err){
 		console.error(err);
 		res.append('status','500');
 		res.send(err);
 	}
-})
- 
+});
+
+app.get('/students/:studentid',async (req,res)=>{
+	const q = `select rowid,fname,lname from students where rowid = ${req.params.studentid}`;
+	try{
+		const results = await query(q);
+		res.json(results);
+	}catch(err){
+		console.error(err);
+		res.append('status','500');
+		res.send(err);
+	}
+});
+
+app.post('/students/',(req,res)=>{
+	console.log(req.body);
+	res.json(req.body);
+});
+
 app.listen(80);
 
 function query(q){
